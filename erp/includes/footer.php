3da3b9eec50a6b5baa19ac56404add1df33170ca
ecommerofficial <ecommerofficial@gmail.com>
2026-07-15 15:07:02 +0530

    footer updated

--- erp/includes/footer.php
+++ erp/includes/footer.php
@@ -1,6 +1,28 @@
 <footer class="footer-panel mt-2 d-flex flex-column flex-md-row align-items-center justify-content-between gap-2">
-    <span>© 2025 Swarnam Jewellers. All rights reserved.</span>
-    <div class="d-flex align-items-center gap-4"><span>Business Date: <strong>20 May 2025</strong> <i
-                class="fa-regular fa-calendar ms-2"></i></span><span>Financial Year: <strong>2024-25</strong> <i
-                class="fa-solid fa-chevron-down ms-2"></i></span></div>
+    <span>© <?php echo date('Y'); ?> Swarnam Jewellers. All rights reserved.</span>
+
+    <div class="d-flex align-items-center gap-4">
+        <span>
+            Business Date:
+            <strong><?php echo date('d M Y'); ?></strong>
+            <i class="fa-regular fa-calendar ms-2"></i>
+        </span>
+
+        <span>
+            Financial Year:
+            <strong>
+                <?php
+                $year = date('Y');
+                $month = date('n');
+
+                if ($month >= 4) {
+                    echo $year . '-' . substr($year + 1, -2);
+                } else {
+                    echo ($year - 1) . '-' . substr($year, -2);
+                }
+                ?>
+            </strong>
+            <i class="fa-solid fa-chevron-down ms-2"></i>
+        </span>
+    </div>
 </footer>
